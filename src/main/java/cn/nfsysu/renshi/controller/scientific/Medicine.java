241458ef3af4f2acb6c0a9c3067c17af9127e6e0
skyisbule <heqiut@qq.com>
2018-01-13 23:09:58 +0800

    完成基础科学类的CRUD

--- src/main/java/cn/nfsysu/renshi/controller/scientific/Medicine.java
+++ src/main/java/cn/nfsysu/renshi/controller/scientific/Medicine.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.PublicKey;
 
 /**
  * Created by skyisbule on 2017/12/15.
