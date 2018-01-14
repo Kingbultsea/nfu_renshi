cb5e8341d7ce9602c406c12972b6e7e0a6a4da0b
skyisbule <heqiut@qq.com>
2018-01-14 20:14:43 +0800

    完成基础技能类的CRUD

---
+++ src/main/java/cn/nfsysu/renshi/controller/view/viewController.java
@@ -0,0 +1,13 @@
+package cn.nfsysu.renshi.controller.view;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by skyisbule on 2018/1/13.
+ * 此控制器用来返回视图  即html页面
+ */
+
+@Controller
+public class viewController {
+
+}
