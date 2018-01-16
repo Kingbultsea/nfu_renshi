473fd39b0412f802f4a03db94d6c648491d1e0b4
skyisbule <heqiut@qq.com>
2018-01-16 22:00:25 +0800

    后台用户部分分页

--- src/main/java/cn/nfsysu/renshi/controller/common/Info.java
+++ src/main/java/cn/nfsysu/renshi/controller/common/Info.java
@@ -1,9 +1,12 @@
 package cn.nfsysu.renshi.controller.common;
 
 import cn.nfsysu.renshi.dao.InfoMapper;
+import cn.nfsysu.renshi.domain.InfoExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by skyisbule on 2017/12/15.
@@ -35,6 +38,16 @@ public class Info {
         if (infoMapper.updateByPrimaryKey(info)==1)
             return "success";
         return "error";
+    }
+
+    @RequestMapping("/get-info-by-page")
+    public List<cn.nfsysu.renshi.domain.Info> getAll(int page){
+        InfoExample e = new InfoExample();
+        e.setOffset(page*10);
+        e.setLimit((page+1)*10);
+        e.setOrderByClause("id");
+        e.createCriteria();
+        return infoMapper.selectByExample(e);
     }
 
 }
